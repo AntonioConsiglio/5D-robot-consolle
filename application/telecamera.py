0e143abb96e62a355e8a18126543a54c53de0fdd
AntonioConsiglio <antonio.mast@hotmail.it>
2022-04-09 10:37:22 +0200

    added mobilenet-ssd inference config

--- application/telecamera.py
+++ application/telecamera.py
@@ -34,10 +34,11 @@ def configure_depth_proprieties(left,right,depth):
 
 class DeviceManager():
 
-    def __init__(self,size,fps):
+    def __init__(self,size,fps,nn_mode):
         self.pipeline = dhai.Pipeline()
         self.size = size
         self.fps = fps
+        self.nn_active = nn_mode
         self._configure_device()
         self.node_list = self.pipeline.getNodeMap()
 
@@ -54,6 +55,11 @@ class DeviceManager():
         xout_rgb = self.pipeline.create(dhai.node.XLinkOut)
         xout_rgb.setStreamName("rgb")
         cam_rgb.preview.link(xout_rgb.input)
+        if self.nn_active:
+            manip,_= self._configure_image_manipulator()
+            cam_rgb.preview.link(manip.inputImage)
+            self._configure_nn_node(manip)
+        return xout_rgb
 
     def _configure_depth_sensor(self):
         monoLeft = self.pipeline.create(dhai.node.MonoCamera)
@@ -65,15 +71,64 @@ class DeviceManager():
         monoLeft.out.link(depth.left)
         monoRight.out.link(depth.right)
         depth.disparity.link(xout_depth.input)
-            
+    
+    def _configure_image_manipulator(self):
+        manip = self.pipeline.create(dhai.node.ImageManip)
+        manipOut = self.pipeline.create(dhai.node.XLinkOut)
+        manipOut.setStreamName('neural_input')
+        manip.initialConfig.setResize(300,300)
+        manip.initialConfig.setFrameType(dhai.ImgFrame.Type.BGR888p)
+        manip.out.link(manipOut.input)
+
+        return manip,manipOut
+
+    def _configure_nn_node(self,manip):
+        
+        nn = self.pipeline.create(dhai.node.MobileNetDetectionNetwork)
+        nnOut = self.pipeline.create(dhai.node.XLinkOut)
+        nnOut.setStreamName("neural")
+        # define nn features
+        nn.setConfidenceThreshold(0.5)
+        nn.setBlobPath("C:\\Users\\anton\\Desktop\\5D-robot-consolle\\neuralNetwork\\mobilenet-ssd_openvino_2021.2_6shave.blob")
+        nn.setNumInferenceThreads(2)
+        # Linking
+        manip.out.link(nn.input)
+        nn.out.link(nnOut.input)
+         
     def enable_device(self):
         self.device_ = dhai.Device(self.pipeline,usb2Mode=True)
-        self.max_disparity = self.node_list[4].initialConfig.getMaxDisparity()
+        if self.nn_active:
+            self.max_disparity = self.node_list[8].initialConfig.getMaxDisparity()
+        else:
+            self.max_disparity = self.node_list[4].initialConfig.getMaxDisparity()
         self._set_output_queue()
     
     def _set_output_queue(self):
         self.q_rgb = self.device_.getOutputQueue("rgb",maxSize = 1,blocking = False)
         self.q_depth = self.device_.getOutputQueue("depth",maxSize = 1,blocking = False)
+        if self.nn_active:
+            self.q_nn = self.device_.getOutputQueue('neural',maxSize=1,blocking=False)
+            self.q_nn_input = self.device_.getOutputQueue('neural_input',maxSize=1,blocking=False)
+    
+    def _normalize_detections(self,detections):
+        det_normal = []
+        for detection in detections:
+            label = detection.label
+            score = detection.confidence
+            xmin,ymin,xmax,ymax = detection.xmin,detection.ymin,detection.xmax,detection.ymax
+            xmin,xmax = int(xmin*self.size[0]),int(xmax*self.size[0])
+            ymin,ymax = int(ymin*self.size[1]),int(ymax*self.size[1])
+            det_normal.append([label,score,[xmin,ymin,xmax,ymax]])
+
+        return det_normal
+    
+    def _write_detections_on_image(self,image,detections):
+        for detection in detections:
+            xmin,ymin,xmax,ymax = detection[2]
+            label = detection[0]
+            score = detection[1]
+            cv2.rectangle(image,(xmin,ymin),(xmax,ymax),(255,255,255),2)
+            cv2.putText(image,f'{label}: {round(score*100,2)} %',(xmin,ymin-10),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
 
     def poll_for_frames(self):
 
@@ -84,28 +139,43 @@ class DeviceManager():
         while not state_frame:
             rgb_foto = self.q_rgb.tryGet()
             depth_frame = self.q_depth.get()
+            if self.nn_active:
+                nn_foto = self.q_nn_input.tryGet()
+                nn_detection = self.q_nn.tryGet()
+                if nn_detection is not None:
+                    detections = nn_detection.detections
+                else:
+                    detections = None
 
             if rgb_foto is not None and depth_frame is not None:
 
                 frames['color_image'] = rgb_foto.getCvFrame()
                 frames['depth_image'] = depth_frame.getFrame() #*(255 /self.max_disparity)).astype(np.uint8)
+                if nn_foto is not None:
+                    frames['nn_input'] = nn_foto.getCvFrame()
+                    if detections is not None:
+                        detections = self._normalize_detections(detections)
+                        self._write_detections_on_image(frames['color_image'],detections)
                 state_frame = True
-                return state_frame,frames
+                return state_frame,frames,detections
             else:
                 frame_count += 1
                 print('empty_frame: ',frame_count)
-                return False,None
+                return False,None,None
 
 if __name__ == '__main__':
 
-    cam = DeviceManager((640,480),25)
+    cam = DeviceManager((640,480),30,nn_mode=True)
     cam.enable_device()
     frame = 0
     while True:
-        stato,frames = cam.poll_for_frames()
+        stato,frames,detections = cam.poll_for_frames()
         if stato:
             cv2.imshow('frame',frames['color_image'])
             cv2.imshow('depth',frames['depth_image'])
+            if 'nn_input' in frames:
+                cv2.imshow('nn_image',frames['nn_input'])
+                print(detections)
             if cv2.waitKey(1) == ord('q'):
                 break
 
